094206293b09eadeaa43be057fc323bf7463f9bb
vesseltov <jwilliameverill@gmail.com>
2024-04-26 13:48:04 -0300

    Reimplementing Arduino functionality into map.js

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -14,28 +14,28 @@ let server = window.location;
     let currentMarker = L.marker(map.getCenter(), { icon: customIcon, autoPan: false }).addTo(map); // Add marker with custom icon;
     let previousMaker = L.marker(map.getCenter());
 
-    // fetching sample data
+    // fetching arduino data
     setInterval(async () => {
         let res = await fetch('/api/test-data');
         let data = await res.json();
         console.log(data);
+
+        if(data.Status === "Success"){
+            map.setView([data.Latitude, data.Longitude], 14);
+            updateMarker([data.Latitude, data.Longitude]);
+        }
     }, 5000);
 
     var layer = protomapsL.leafletLayer({url: `${server}/api/maps/north_halifax.pmtiles`, theme:'light'});
     layer.addTo(map);
 
-    // L.tileLayer('../static/maps/{z}/{x}/{y}.png', {
-    //     maxZoom: 19,
-    //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    // }).addTo(map);
-
     const campusMarker = L.marker([44.66941024799195, -63.61346907985597]).addTo(map)
         .bindPopup('NSCC IT Campus')
         .openPopup();
 
-    const updateMarker = () => {
+    const updateMarker = (coordinates) => {
         previousMaker.setLatLng(new L.LatLng(currentMarker._latlng.lat, currentMarker._latlng.lng));
-        currentMarker.setLatLng(map.getCenter()); // Update marker position to the new center
+        currentMarker.setLatLng(new L.LatLng(coordinates[0], coordinates[1])); // Update marker position to the new center
         // Calculate new bearing
         let deltaLat = currentMarker._latlng.lat - previousMaker._latlng.lat;
         let deltaLng = currentMarker._latlng.lng - previousMaker._latlng.lng;
@@ -45,8 +45,6 @@ let server = window.location;
         }
         currentMarker.setRotationAngle(currentBearing);
     };
-
-    map.on('moveend', updateMarker); // Listen for map move end event
 
     setInterval(() => {
         map.invalidateSize(); // Refresh map size to ensure correct centering
