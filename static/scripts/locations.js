a8eacc3aab86625f4f07e8904128f268ca1a9dd1
vesseltov <jwilliameverill@gmail.com>
2024-05-02 10:38:22 -0300

    Selecting routes from a dropdown list

---
+++ static/scripts/locations.js
@@ -0,0 +1,27 @@
+var locations = [
+    {
+        "name": "NSCC Institute of Technology",
+        "lat": "44.66921496139025",
+        "lng": "-63.61343893200053"
+    },
+    {
+        "name": "NSCC Ivany Campus",
+        "lat": "44.654137896484315",
+        "lng": "-63.55199202097848"
+    },
+    {   
+        "name": "NSCC Akerley Campus",
+        "lat": "44.68308019268081",
+        "lng": "-63.5267217711997"
+    },
+    {
+        "name": "Fun & Shakes",
+        "lat": "44.643306004587345",
+        "lng": "-63.5704407608375"
+    },
+    {
+        "name": "Scotiabank Theatre Halifax",
+        "lat": "44.64959057496531",
+        "lng": "-63.67134104919394"
+    }
+];
--- static/scripts/map.js
+++ static/scripts/map.js
@@ -2,11 +2,11 @@
 
     let server = window.location;
 
-        const coordinatesTag = document.querySelector("#coordinates");
-        const direction = document.querySelector("#direction");
+    const coordinatesTag = document.querySelector("#coordinates");
+    const direction = document.querySelector("#direction");
 
-        coordinatesTag.textContent = "Loading";
-        direction.textContent = "Loading";
+    coordinatesTag.textContent = "Loading";
+    direction.textContent = "Loading";
 
     // create the custom icon
     const customIcon = L.icon({
@@ -16,25 +16,73 @@
         popupAnchor: [0, -16] 
     });
 
+    L.Control.Dropdown = L.Control.extend({
+        onAdd: map => {
+            var selectList = L.DomUtil.create("select");
+
+            defaultText = L.DomUtil.create("option");
+            defaultText.text = "Select a destination";
+            defaultText.value = -1;
+            defaultText.disabled = true;
+            defaultText.hidden = true;
+            selectList.appendChild(defaultText);
+            selectList.value = -1;
+
+            for (var i = 0; i < locations.length; i++) {
+                var opt = L.DomUtil.create("option");
+                opt.text = locations[i].name;
+                opt.value = locations.map(e => e.name).indexOf(locations[i].name);
+                selectList.appendChild(opt);
+            }
+
+            selectList.onchange = function(){updateRoute()};
+
+            return selectList;
+        },
+        onRemove: map => {
+    
+        }
+    });
+    L.control.dropdown = function(opts) {
+        return new L.Control.Dropdown(opts);
+    }
+
+    function updateRoute() {
+        if (routeStart._container.value != -1 && routeEnd._container.value != -1) {
+            var start = locations[routeStart._container.value];
+            var end = locations[routeEnd._container.value];
+            // program craps itself if start + destination are the same
+            waypoints = [
+                L.latLng(start.lat, start.lng),
+                L.latLng(end.lat, end.lng)
+            ];
+            if (waypoints[0].lat !== waypoints[1].lat || waypoints[0].lng !== waypoints[1].lng) {
+                mapRouting.setWaypoints(waypoints);
+                mapRouting.route();
+            }
+        }
+    }
+    
     let map = L.map('map', { zoomControl: false });
 
     // this will create the map and set the view
-    map.setView([44.650627, -63.597140], 16)
+    map.setView([44.650627, -63.597140], 16);
 
     // create 2 markers, one for the user, one for calculating angle
     let currentMarker = L.marker(map.getCenter(), { icon: customIcon, autoPan: false }).addTo(map); // Add marker with custom icon;
     let previousMaker = L.marker(map.getCenter());
 
     // basic route nonsense
-    L.Routing.control({
-        waypoints: [
-          L.latLng(44.6697021161829, -63.6133868698228),
-          L.latLng(44.66436078522418, -63.60027573010271)
-        ],
+    const mapRouting =  L.Routing.control({
         // FIXME: currently only detects a server running under localhost:5500
         serviceUrl: 'http://localhost:5500/route/v1',
+        fitSelectedRoutes: false,
         position: 'bottomright'
     }).addTo(map);
+
+    // add our new custom element
+    var routeEnd = L.control.dropdown({ position: 'bottomright' }).addTo(map);
+    var routeStart = L.control.dropdown({ position: 'bottomright' }).addTo(map);
 
     // fetching arduino data
     setInterval(async () => {
@@ -50,11 +98,6 @@
 
     var layer = protomapsL.leafletLayer({url: `${server}api/maps/nova_scotia.pmtiles`, theme:'light'});
     layer.addTo(map);
-
-    // this is the marker for the campus
-    const campusMarker = L.marker([44.66941024799195, -63.61346907985597]).addTo(map)
-        .bindPopup('NSCC IT Campus')
-        .openPopup();
 
     /**
      * This function will update the coordinates for the user marker.
